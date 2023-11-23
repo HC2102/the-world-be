abf4b4918f48f34c7bf56690efe3b2eac46bfbcf
TruongPD <truongpd2106@gmail.com>
2023-11-23 22:02:44 +0700

    project init

---
+++ src/main/java/he170417/prj005/theWorld/TheWorldApplication.java
@@ -0,0 +1,13 @@
+package he170417.prj005.theWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/he170417/prj005/theWorld/TheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package he170417.prj005.theWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
